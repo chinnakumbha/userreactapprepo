a4221930c315d67b5d55a6b057205f37190f0911
sahoo <sahil.thakor@ey.com>
2022-12-20 23:53:41 +0530

    Task pilot code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Tasks from './tasks/Tasks'
 
+// we can give any name but let's give it as "Tasks"
+
+//sometimes automatically IDE will suggest
+// Tasks.js is under tasks folder
+// it will start with import keyword
+
+// import we need to take the folder path into consideration
 function App() {
   return (
-  <p>this is todoapp developing ..</p> 
+    <Tasks/> 
+  // now we're using this Tasks variable as a html tag, 
+  // we can say it is our custom html tag we created
   );
 }
 
---
+++ src/tasks/Tasks.js
@@ -0,0 +1,15 @@
+// i will write code here 
+// this will be a base syntax, no need to worry about syntax as of now
+
+// const is keyword for declaring variable
+
+// Tasks is variable name which is storing this html code (h2 tag...)
+// Tasks we can give any name, not nessesary to give exact name as file name, but mostly we give same as file name and Capitale first letter
+
+// so we have to export at the end so other files can use it inside their code
+const Tasks = () => {
+    return <h2>Here Tasks will come.</h2>
+}
+
+export default Tasks
+
